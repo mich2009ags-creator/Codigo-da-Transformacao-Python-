3f495e3f5d6daa40e5fe21de31a0f6190dfbb50b
Michelle <mich2009ags@gmail.com>
2026-05-16 15:16:09 -0300

    Update and rename Módulo 010/Módulo 10 to Clima.py

--- Clima.py
+++ Clima.py
@@ -1,7 +1,7 @@
 import requests
 
 # 🔑 Coloque sua chave da API aqui
-API_KEY = "SUA_CHAVE_AQUI"
+API_KEY = "fcf365f899b8f8cde217ce11781e046c "
 CIDADE = "Sao Paulo"
 URL = f"http://api.openweathermap.org/data/2.5/weather?q={CIDADE}&appid={API_KEY}&lang=pt_br&units=metric"
 
@@ -40,22 +40,3 @@ buscar_clima()
 
 import requests
 
-API_KEY = "SUA_CHAVE_TMDB"
-FILME = "Inception"
-
-url = f"https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&query={FILME}&language=pt-BR"
-
-try:
-    resposta = requests.get(url)
-    resposta.raise_for_status()
-    dados = resposta.json()
-
-    filme = dados["results"][0]
-
-    print("\n🎬 Filme:")
-    print(f"Título: {filme['title']}")
-    print(f"Sinopse: {filme['overview']}")
-    print(f"Data de lançamento: {filme['release_date']}")
-
-except Exception as e:
-    print(f"Erro: {e}")
